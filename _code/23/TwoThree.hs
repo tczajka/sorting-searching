5481cb7cde7dd28d944843714a5b943f3b2b15a5
Tomek Czajka <tczajka@gmail.com>
2020-05-23 01:45:21 -0700

    2-3 trees

--- _code/23/TwoThree.hs
+++ _code/23/TwoThree.hs
@@ -1,61 +1,55 @@
-data Tree a =
+data Tree t =
     Empty
-  | Leaf a
-  | Node2 Int a (Tree a) (Tree a)          -- Node2 height smallest a b
-  | Node3 Int a (Tree a) (Tree a) (Tree a) -- Node3 height smallest a b c
+  | Leaf t
+  | Node2 Int t (Tree t) (Tree t)          -- Node2 height smallest a b
+  | Node3 Int t (Tree t) (Tree t) (Tree t) -- Node3 height smallest a b c
 
-height :: Tree a -> Int
+height :: Tree t -> Int
 height (Leaf _) = 0
 height (Node2 h _ _ _) = h
 height (Node3 h _ _ _ _) = h
 
-smallest :: Tree a -> a
+smallest :: Tree t -> t
 smallest (Leaf x) = x
 smallest (Node2 _ s _ _) = s
 smallest (Node3 _ s _ _ _) = s
 
-toList :: Tree a -> [a]
-toList Empty = []
-toList (Leaf x) = [x]
-toList (Node2 _ _ a b) = toList a ++ toList b
-toList (Node3 _ _ a b c) = toList a ++ toList b ++ toList c
-
 -- combine children into a node
-node2 :: Tree a -> Tree a -> Tree a
+node2 :: Tree t -> Tree t -> Tree t
 node2 a b = Node2 (height a + 1) (smallest a) a b
 
-node3 :: Tree a -> Tree a -> Tree a -> Tree a
+node3 :: Tree t -> Tree t -> Tree t -> Tree t
 node3 a b c = Node3 (height a + 1) (smallest a) a b c
 
 -- combine 2 to 4 trees of height h to make 1 or 2 trees of height h+1
-levelUp :: [Tree a] -> [Tree a]
+levelUp :: [Tree t] -> [Tree t]
 levelUp [a,b] = [node2 a b]
 levelUp [a,b,c] = [node3 a b c]
 levelUp [a,b,c,d] = [node2 a b, node2 c d]
 
 -- Merge two nonempty trees.
 -- Returns 1 or 2 trees of height = maximum height of the two trees.
-mergeToList :: Tree a -> Tree a -> [Tree a]
-mergeToList a b
+mergeToSameHeight :: Tree t -> Tree t -> [Tree t]
+mergeToSameHeight a b
   | height a < height b =
     case b of
-      Node2 _ _ b1 b2 -> levelUp (mergeToList a b1 ++ [b2])
-      Node3 _ _ b1 b2 b3 -> levelUp (mergeToList a b1 ++ [b2, b3])
+      Node2 _ _ b1 b2 -> levelUp (mergeToSameHeight a b1 ++ [b2])
+      Node3 _ _ b1 b2 b3 -> levelUp (mergeToSameHeight a b1 ++ [b2, b3])
   | height a > height b =
     case a of
-      Node2 _ _ a1 a2 -> levelUp ([a1] ++ mergeToList a2 b)
-      Node3 _ _ a1 a2 a3 -> levelUp ([a1,a2] ++ mergeToList a3 b)
+      Node2 _ _ a1 a2 -> levelUp ([a1] ++ mergeToSameHeight a2 b)
+      Node3 _ _ a1 a2 a3 -> levelUp ([a1,a2] ++ mergeToSameHeight a3 b)
   | otherwise = [a, b]
 
-merge :: Tree a -> Tree a -> Tree a
+merge :: Tree t -> Tree t -> Tree t
 merge a Empty = a
 merge Empty b = b
 merge a b =
-  case mergeToList a b of
+  case mergeToSameHeight a b of
     [t] -> t
     [t, u] -> node2 t u
 
-split :: (a -> Bool) -> Tree a -> (Tree a, Tree a)
+split :: (t -> Bool) -> Tree t -> (Tree t, Tree t)
 split _ Empty = (Empty, Empty)
 
 split f (Leaf x)
@@ -76,25 +70,34 @@ split f (Node3 _ _ a b c)
   | otherwise =
     let (c1,c2) = split f c in (merge (node2 a b) c1, c2)
 
-contains :: Ord a => Tree a -> a -> Bool
-contains t x =
-  case split (>= x) t of
+contains :: Ord t => Tree t -> t -> Bool
+contains a x =
+  case split (>= x) a of
     (_, Empty) -> False
-    (_, t) -> smallest t == x
+    (_, a2) -> smallest a2 == x
 
-insert :: Ord a => Tree a -> a -> Tree a
-insert t x =
-  let (t1, t2) = split (>= x) t
-  in t1 `merge` (Leaf x) `merge` t2
+insert :: Ord t => Tree t -> t -> Tree t
+insert a x =
+  let (a1, a2) = split (>= x) a
+  in a1 `merge` (Leaf x) `merge` a2
 
-delete :: Ord a => Tree a -> a -> Tree a
-delete t x =
-  let (t1, t2) = split (>= x) t
-      (_, t3) = split (>x) t2
-  in merge t1 t3
+delete :: Ord t => Tree t -> t -> Tree t
+delete a x =
+  let (a1, a2) = split (>= x) a
+      (_, a3) = split (>x) a2
+  in merge a1 a3
 
-fromList :: Ord a => [a] -> Tree a
+fromList :: Ord t => [t] -> Tree t
 fromList = foldl insert Empty
+
+prepend :: Tree t -> [t] -> [t]
+prepend Empty xs = xs
+prepend (Leaf x) xs = x : xs
+prepend (Node2 _ _ a b) xs = prepend a (prepend b xs)
+prepend (Node3 _ _ a b c) xs = prepend a (prepend b (prepend c xs))
+
+toList :: Tree t -> [t]
+toList a = prepend a []
 
 main :: IO ()
 main = do
